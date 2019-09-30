0cb4e3588c528fef1928b4aadbe6feccd9128b4b
genEmprendedor <46355001+genEmprendedor@users.noreply.github.com>
2019-09-30 12:08:59 -0600

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+onst countdown = document.querySelector('.countdown');
+
+//Set Launch Date
+const launchDate = new Date('Nov 1, 2019 13:00:00').getTime();
+
+//Update every second
+const intvl = setInterval(() => {
+  //Get today's date and time
+  const now = new Date().getTime();
+  
+  //Distance between now and target date
+  const distance = launchDate - now;
+  
+  //Calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  //display results
+  countdown.innerHTML = `
+    <div>${days}<span>Days</span></div>
+    <div>${hours}<span>Hours</span></div>
+    <div>${mins}<span>Minutes</span></div>
+    <div>${seconds}<span>Seconds</span></div>
+  `;
+  
+  //Past launch date?
+  if(distance <0) {
+    //Stop countdown
+    clearInterval(intvl);
+    //Style and output text
+    countdown.style.color = '#17a2b8';
+    countdown.innerHTML = 'Launched!';
+  }
+}, 1000);
